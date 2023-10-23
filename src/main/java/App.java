cbb552dcfce75c6a53e4d7935f9a93493f8c7c32
DMQ <116147058+quan29117@users.noreply.github.com>
2023-10-23 07:15:06 +0700

    menu, search GUI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Search_Q.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("search.fxml"));
             Scene scene = new Scene(root);
             stage.setTitle("Translator");
             stage.setScene(scene);
